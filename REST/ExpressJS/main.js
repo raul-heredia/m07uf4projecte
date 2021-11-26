4d87aeaded71aad476cde2b29ff9c714a4ee68bf
macasa <15585369.clot@fje.edu>
2021-11-26 18:41:59 +0100

    Finalitzada logica

--- REST/ExpressJS/main.js
+++ REST/ExpressJS/main.js
@@ -11,6 +11,7 @@ app.use(express.json()) // per analitzar les peticions HTTP que portin JSON al b
 // D = Diamantes
 // C = Corazones
 // P = Picas
+// Declarem una constant de la baralla
 const baralla = [
     { nom: 'AST', value: 1 },
     { nom: '2T', value: 2 },
@@ -75,71 +76,73 @@ let partides = [];
 
 function isPartidaGet(idPartida, req, res) {
     let checkPartida = partides.find(a => a.codiPartida === idPartida); // Busquem la partida en el array partides
-    if (!checkPartida) {
+    if (!checkPartida) { // Si no existeix retornem un 404
         res.status(404, 'error'); // Si no troba partida retorna un 404
-        res.send("Error, no s'ha trobat cap partida amb identificador: " + idPartida)
+        res.send("Error, no s'ha trobat cap partida amb identificador: " + idPartida);
     }
 }
 
-
+function isPartidaFinalitzada(numPartida, req, res){
+    let partida = partides.find(x => x.codiPartida === numPartida); // Busquem la partida que el seu codiPartida sigui igual a numPartida
+    if (partida.torn === -1){
+        res.status(404, 'error'); // Si no troba partida retorna un 404
+        res.send(`No pots demanar més cartes, la partida ${numPartida} ha finalitzat`);
+    }
+    
+}
 
 function pideJugador(numPartida, req, res) {
-    let partida = partides.find(x => x.codiPartida === numPartida);
+    let partida = partides.find(x => x.codiPartida === numPartida); // Busquem la partida que el seu codiPartida sigui igual a numPartida
     let carta = Math.floor(Math.random() * partida.numCarta);  // genera un numero aleatorio de 0 a numCarta (La primera vez vale 13 por lo que genera de 0 a 12)
-    let isFirst = false;
-    // Si el total del jugador es menor a 11 nos interesa que el valor de AS sea 11, en caso contrario seguirá valiendo 1.
-    if (partida.torn === 1) {
-        isFirst = true;
-        for (let i = 0; i < 2; i++) {
-            jugadorAs(partida, carta);
-            console.log(`Jugador: Ha sortit la carta ${partida.cartes[carta].nom}`)
-            partida.jugadorSum += partida.cartes[carta].value;
-            partida.cartesJugador.push(partida.cartes[carta]);
-            partida.numCarta -= 1;
-            partida.cartes.splice(carta, 1);
-            if (i == 0) carta = Math.floor(Math.random() * partida.numCarta);
+    
+    if (partida.torn === 1) { // si es el primer torn 
+        for (let i = 0; i < 2; i++) { // demanem dos cartes ja que la primera vegada es donen dues cartes
+            jugadorAs(partida, carta); // Si el total del jugador es menor a 11 nos interesa que el valor de AS sea 11, en caso contrario seguirá valiendo 1.
+            console.log(`Jugador: Ha sortit la carta ${partida.cartes[carta].nom}`) // printem per consola la carta que ha sortit
+            partida.jugadorSum += partida.cartes[carta].value; // sumem el valor de la carta a la suma total del jugador
+            partida.cartesJugador.push(partida.cartes[carta]); // afegim la carta que ha sortit al array de cartes del jugador
+            partida.numCarta -= 1; // Restem el numero total de cartes
+            partida.cartes.splice(carta, 1); // Traiem la carta del arrai
+            if (i == 0) carta = Math.floor(Math.random() * partida.numCarta); // Si el valor de i val 0, traiem un altre número random
         }
+        res.send(partida.cartesJugador) // retornem el total de cartes, ja que només hi haurà les dues primeres
     } else {
-        jugadorAs(partida, carta);
-        console.log(`Jugador: Ha sortit la carta ${partida.cartes[carta].nom}`)
-        res.send(partida.cartes[carta]);
-        partida.jugadorSum += partida.cartes[carta].value;
-        partida.cartesJugador.push(partida.cartes[carta]);
-        partida.numCarta -= 1;
-        partida.cartes.splice(carta, 1);
+        jugadorAs(partida, carta); // Si el total del jugador es menor a 11 nos interesa que el valor de AS sea 11, en caso contrario seguirá valiendo 1.
+        console.log(`Jugador: Ha sortit la carta ${partida.cartes[carta].nom}`) // Printem la carta per consola
+        res.send(partida.cartes[carta]); // Retornem la carta que ha sortit al jugador
+        partida.jugadorSum += partida.cartes[carta].value; // sumem el valor de la carta al total de la suma del jugador
+        partida.cartesJugador.push(partida.cartes[carta]); // Afegim la carta que ha sortit al array de cartes del jugador
+        partida.numCarta -= 1; // restem el numero de cartes total
+        partida.cartes.splice(carta, 1); // Eliminem la carta del array de cartes total
     }
-    partida.torn++
-    if (isFirst) res.send(partida.cartesJugador);
+    partida.torn++ // sumem el torn
 }
 
 function jugadorAs(partida, carta) {
-    if (partida.cartes[carta].value == 1) {
-        if (partida.jugadorSum < 11) {
-            partida.cartes[carta].value = 11
+    if (partida.cartes[carta].value == 1) { // si el value de la carta es 1 (Un as)
+        if (partida.jugadorSum < 11) { // si la suma de punts es menor a 11 ens interessa que el as valgui 11
+            partida.cartes[carta].value = 11 // modifiquem el value del as que ha sortit per tal de que valgui 11
         }
     }
 }
 
 function crupier(numPartida) {
-    let partida = partides.find(x => x.codiPartida === numPartida);
+    let partida = partides.find(x => x.codiPartida === numPartida); // Busquem la partida que el seu codiPartida sigui igual a numPartida
     let carta = Math.floor(Math.random() * partida.numCarta);  // genera un numero aleatorio de 0 a numCarta (La primera vez vale 13 por lo que genera de 0 a 12)
-    let isFirst = false;
 
-    // Si el total del crupier es mayor a 17, se planta y no sigue cogiendo cartas.   
-    if (partida.crupierSum < 17) {
-        if (partida.torn === 1) {
-            isFirst = true;
-            for (let i = 0; i < 2; i++) {
-                crupierAs(partida, carta);
-                console.log(`Crupier: Ha sortit la carta ${partida.cartes[carta].nom}`)
-                partida.crupierSum += partida.cartes[carta].value;
-                partida.cartesCrupier.push(partida.cartes[carta]);
-                partida.numCarta -= 1;
-                partida.cartes.splice(carta, 1);
-                if (i == 0) carta = Math.floor(Math.random() * partida.numCarta);
+    if (partida.crupierSum < 17) { // El crupier només rep una carta si el seu total es menor a 17
+        if (partida.torn === 1) { // si es la primera vegada
+            for (let i = 0; i < 2; i++) { // obté dues cartes
+                crupierAs(partida, carta); // Si el total del crupier es mayor a 17, se planta y no sigue cogiendo cartas. 
+                console.log(`Crupier: Ha sortit la carta ${partida.cartes[carta].nom}`) // printem per consola la carta que ha sortit
+                partida.crupierSum += partida.cartes[carta].value; // sumem el value de la carta a la suma total del crupier
+                partida.cartesCrupier.push(partida.cartes[carta]); // pujem la carta a les cartes del crupier
+                partida.numCarta -= 1; // disminuim el numero de cartes del array de cartes
+                partida.cartes.splice(carta, 1); // eliminem la carta del array
+                if (i == 0) carta = Math.floor(Math.random() * partida.numCarta); // si es la primera volta del bucle generem un altre número random
             }
         } else {
-            crupierAs(partida, carta);
+            crupierAs(partida, carta); // Si el total del crupier es mayor a 17, se planta y no sigue cogiendo cartas. 
             console.log(`Crupier: Ha sortit la carta ${partida.cartes[carta].nom}`)
             partida.crupierSum += partida.cartes[carta].value;
             partida.cartesCrupier.push(partida.cartes[carta]);
@@ -150,14 +153,43 @@ function crupier(numPartida) {
 }
 
 function crupierAs(partida, carta) {
-    if (partida.cartes[carta].value == 1) {
-        if (partida.crupierSum < 11) {
-            partida.cartes[carta].value = 11
+    if (partida.cartes[carta].value == 1) { // si el value de la carta es 1 (Un as)
+        if (partida.crupierSum < 11) { // si el total del crupier es menor a 11 ens interessa que el as valgui 11
+            partida.cartes[carta].value = 11 // posem que el valor del as que ha sortit es igual a 11
         }
     }
 }
 
+function guanya(res){
+    console.log('Has guanyat');
+    res.send ('Has guanyat');
+}
+function perd(res){
+    console.log('Has perdut');
+    res.send ('Has perdut');
+}
 
+
+function comprovaResultat(numPartida, req, res){
+    let partida = partides.find(x => x.codiPartida === numPartida); // Busquem la partida que el seu codiPartida sigui igual a numPartida
+    partida.torn = -1;
+    if (partida.crupierSum < partida.jugadorSum && partida.jugadorSum < 21){
+        console.log("Guanya primer if");
+        guanya(res);
+    }else if (partida.crupierSum > 21 && partida.jugadorSum < 21){
+        console.log("Guanya segon if");
+        guanya(res);
+    }else if(partida.jugadorSum == 21){
+        console.log("Guanya tercer if");
+        guanya(res);
+    }else if(partida.crupierSum == 21){
+        console.log("Perd quart if");
+        perd(res);
+    }else if(partida.jugadorSum > 21){
+        console.log("Perd cinqué if");
+        perd(res);
+    }
+}
 app.get('/', (req, res) => res.send('Hello World!'));
 
 app.post('/crearPartida', (req, res) => {
@@ -174,6 +206,7 @@ app.get('/:codiPartida/detallsPartida', (req, res) => {
 app.get('/:codiPartida/obtenirCarta', (req, res) => {
     let idPartida = parseInt(req.params.codiPartida);
     isPartidaGet(idPartida, req, res);
+    isPartidaFinalitzada(idPartida, req, res)
     crupier(idPartida)
     pideJugador(idPartida, req, res);
 });
@@ -187,6 +220,22 @@ app.get('/:codiPartida/mostraCartesCrupier', (req, res) => {
     let idPartida = parseInt(req.params.codiPartida);
     isPartidaGet(idPartida, req, res);
     res.send(partides.find(a => a.codiPartida === parseInt(req.params.codiPartida)).cartesCrupier);
+});
+app.put ('/:codiPartida/plantarse', (req, res) => {
+    let idPartida = parseInt(req.params.codiPartida);
+    let partida = partides.find(x => x.codiPartida === idPartida); // Busquem la partida que el seu codiPartida sigui igual a numPartida
+    isPartidaGet(idPartida, req, res);
+    isPartidaFinalitzada(idPartida, req, res);
+    while (partida.crupierSum  < 17) crupier(idPartida, req, res); // mentre que sigui menor a 17 demana cartes
+    comprovaResultat(idPartida, req, res);
+});
+app.delete ('/:codiPartida/acabaPartida', (req, res) => {
+    let idPartida = parseInt(req.params.codiPartida);
+    let partida = partides.find(x => x.codiPartida === idPartida); // Busquem la partida que el seu codiPartida sigui igual a numPartida
+    let index = partides.findIndex(obj => obj.codiPartida == idPartida);
+    isPartidaGet(idPartida, req, res);
+    partides.splice(index, 1);
+    res.send(`Partida ${idPartida} eliminada`);
 });
 
 app.listen(3000, () => console.log('inici servidor'));
